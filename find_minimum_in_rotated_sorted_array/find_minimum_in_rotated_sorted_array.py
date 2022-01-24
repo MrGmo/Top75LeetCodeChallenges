c0c7257ae1e07df4ba8102d210f7dd4b8aba61bf
Guillermo <gaguilar128@gmail.com>
2022-01-24 15:57:53 -0600

    find minimum in rotated array challenge

---
+++ find_minimum_in_rotated_sorted_array/find_minimum_in_rotated_sorted_array.py
@@ -0,0 +1,17 @@
+def findMin(self, nums):
+    result = nums[0]
+    l, r = 0, len(nums) - 1
+
+    while l <= r:
+        if nums[l] < nums[r]:
+            result = min(result, nums[l])
+            break
+
+        m = (l + r) // 2
+        result = min(result, nums[m])
+
+        if nums[m] >= nums[l]:
+            l = m + 1
+        else:
+            r = m - 1
+    return result
